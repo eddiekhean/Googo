cd7a58a9ac685b2f656a935e3b0636af85482ac1
Lê Duy Khang <lehoanggiakhuong2006@gmail.com>
2025-10-19 16:45:50 +0700

    Add initial Spring Cloud API Gateway service
    Introduces a new Spring Boot project for an API Gateway using Spring Cloud Gateway and WebFlux. Includes JWT utility for token validation, Redis support, configuration files, and Maven wrapper scripts. Sets up a basic route for user-service and provides initial test scaffolding.

---
+++ backend/api-gateway/src/main/java/com/example/apigateway/util/JwtUtil.java
@@ -0,0 +1,80 @@
+package com.example.apigateway.util;
+
+
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+
+import java.nio.file.Files;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+
+@Component
+public class JwtUtil {
+
+    @Getter
+    @Value("${jwt.access-expiration}")
+    private long accessExpiration;
+
+    @Value("${jwt.refresh-expiration}")
+    private long refreshExpiration;
+
+    private final PublicKey publicKey;
+
+    public JwtUtil(
+            @Value("${jwt.public-key-path}") String publicKeyPath
+    ) throws Exception {
+        this.publicKey = loadPublicKey(publicKeyPath);
+    }
+
+    private PrivateKey loadPrivateKey(String path) throws Exception {
+        var file = ResourceUtils.getFile(path);
+        String key = new String(Files.readAllBytes(file.toPath()))
+                .replaceAll("-----BEGIN ([A-Z ]+)-----", "")
+                .replaceAll("-----END ([A-Z ]+)-----", "")
+                .replaceAll("\\s+", "");
+        byte[] decoded = Base64.getDecoder().decode(key);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+        return KeyFactory.getInstance("RSA").generatePrivate(spec);
+    }
+
+    private PublicKey loadPublicKey(String path) throws Exception {
+        var file = ResourceUtils.getFile(path);
+        String key = new String(Files.readAllBytes(file.toPath()))
+                .replaceAll("-----BEGIN ([A-Z ]+)-----", "")
+                .replaceAll("-----END ([A-Z ]+)-----", "")
+                .replaceAll("\\s+", "");
+        byte[] decoded = Base64.getDecoder().decode(key);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
+        return KeyFactory.getInstance("RSA").generatePublic(spec);
+    }
+
+
+    // --- Verify token ---
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(publicKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            extractClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
